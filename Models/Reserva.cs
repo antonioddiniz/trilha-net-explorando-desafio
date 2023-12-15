f0582e8ef41f6981c1d54e2952802b9b9f017948
Antonio Diniz <paduajur@gmail.com>
2023-12-15 14:09:06 -0300

    Desafio concluído

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -15,16 +15,18 @@ namespace DesafioProjetoHospedagem.Models
 
         public void CadastrarHospedes(List<Pessoa> hospedes)
         {
-            // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
-            // *IMPLEMENTE AQUI*
-            if (true)
+            if (hospedes.Count <= Suite.Capacidade)
             {
                 Hospedes = hospedes;
+                foreach(Pessoa hospede in hospedes)
+                {
+                    Console.WriteLine($"Hospede:{hospede.Nome}");   
+                }
+                Console.WriteLine("Usuários cadastrados com sucesso");
             }
             else
             {
-                // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
-                // *IMPLEMENTE AQUI*
+                Console.WriteLine($"Você está tentando cadastrar {hospedes.Count} em uma suíte que tem capacidade para {Suite.Capacidade}");
             }
         }
 
@@ -35,25 +37,21 @@ namespace DesafioProjetoHospedagem.Models
 
         public int ObterQuantidadeHospedes()
         {
-            // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
-            // *IMPLEMENTE AQUI*
-            return 0;
+            return Hospedes.Count;
         }
 
         public decimal CalcularValorDiaria()
         {
-            // TODO: Retorna o valor da diária
-            // Cálculo: DiasReservados X Suite.ValorDiaria
-            // *IMPLEMENTE AQUI*
             decimal valor = 0;
-
-            // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
-            // *IMPLEMENTE AQUI*
-            if (true)
+            if (DiasReservados >= 10)
             {
-                valor = 0;
+                decimal desconto = 0.9M;
+                valor = DiasReservados * Suite.ValorDiaria * desconto;
             }
-
+            else 
+            {
+                valor = DiasReservados * Suite.ValorDiaria;
+            }
             return valor;
         }
     }
